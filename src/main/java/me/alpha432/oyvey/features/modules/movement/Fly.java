8acba2152386a748736b30a00e467be47ded5469
Yase Emulator <janigrosbaltrousmr@gmail.com>
2026-02-24 19:01:23 +0100

    Create Fly.java

---
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/Fly.java
@@ -0,0 +1,38 @@
+package me.alpha432.oyvey.features.modules.movement;
+
+import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.settings.Setting;
+import net.minecraft.client.player.LocalPlayer;
+
+public class FlyModule extends Module {
+    private final Setting<Float> speed = num("Speed", 2f, 0.1f, 10f);
+
+    public FlyModule() {
+        super("Fly", "fly..", Category.MOVEMENT);
+    }
+
+    @Override
+    public void onEnable() {
+        if (nullCheck()) return;
+        mc.player.getAbilities().flying = true;
+        mc.player.getAbilities().mayfly = true;
+        mc.player.onUpdateAbilities();
+    }
+
+    @Override
+    public void onDisable() {
+        if (nullCheck()) return;
+        mc.player.getAbilities().flying = false;
+        mc.player.getAbilities().mayfly = false;
+        mc.player.getAbilities().flyingSpeed = 0.05f;
+        mc.player.onUpdateAbilities();
+    }
+
+    @Override
+    public void onTick() {
+        if (nullCheck()) return;
+        mc.player.getAbilities().mayfly = true;
+        mc.player.getAbilities().flyingSpeed = speed.getValue() * 0.05f;
+        mc.player.onUpdateAbilities();
+    }
+}
